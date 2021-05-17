0a6c813ca0f3a4c7b2e084fefe181d076a4a3e60
Yordan Yordanov <sshymi@yahoo.com>
2021-05-17 09:26:02 +0300

    Base spring initializr project

---
+++ src/main/java/com/test/colorchooser/ColorChooserApplication.java
@@ -0,0 +1,13 @@
+package com.test.colorchooser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorChooserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorChooserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/colorchooser/ColorChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.colorchooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorChooserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
